f96a424b2fbb8a2231b99700d7348c0b85271f2e
YANAGIDA Yuki <ynagi2@dhcp219-129.slis.tsukuba.ac.jp>
2021-11-15 15:08:24 +0900

    add middle report

--- exhibition.py
+++ exhibition.py
@@ -1,27 +1,34 @@
 import streamlit as st
-import pandas as pd
-from PIL import Image
+import os
+import re
+import base64
+
+
+def show_chapter(texts_dir, text_files, idx):
+    content = ""
+    with open(texts_dir + text_files[idx]) as f:
+        for line in f:
+            if re.search(r'^fig', line):
+                st.markdown(content + "\n")
+                content = ""
+                image_name = line.strip()
+                with open("./images/" + image_name + ".png", "rb") as im:
+                    image = im.read()
+                data_url = base64.b64encode(image).decode("utf-8")
+                image_source = '<img src="data:./images/' + image_name + '/png;base64,' + data_url + '\" width="400" style="display: block; margin: auto;">'
+                st.markdown(image_source, unsafe_allow_html=True)
+            else:
+                content += line
+    st.markdown(content + "\n")
 
 
 def main():
-    st.title('コロナ禍におけるライブコンサートの変化（仮）')
-    # st.table(pd.DataFrame({
-    #     'first column': [1, 2, 3, 4],
-    #     'second column': [10, 20, 30, 40]
-    # }))
-    sections = ("はじめに", "1章 コロナ以前", "2章 オンライン", "3章 後の対面", "おわりに")
+    st.title("コロナ禍におけるライブコンサートの変化（仮）")
+    texts_dir = "./text/"
+    text_files = sorted(os.listdir(texts_dir))
+    sections = ("はじめに", "1章 コロナ禍以前・通常のライブ", "2章 コロナ禍の無観客・配信ライブ", "3章 後の対面", "おわりに")
     add_selectbox = st.sidebar.selectbox("章立て", sections)
-    if add_selectbox == "はじめに":
-        st.markdown('# はじめに\nhogehoge')
-    elif add_selectbox == "1章 コロナ以前":
-        concert_tuple = ("あいうえお", "abcde")
-        st.markdown("# コロナ以前のはなし\n")
-        concert = st.selectbox("知りたい公演", concert_tuple)
-        st.write('公演:', concert)
-    elif add_selectbox == "2章 オンライン":
-        image = Image.open("./job_gakugeiin_woman.png")
-        st.image(image, caption="画像はこんな感じ(いらすとや より．実際はちゃんと出典を書きます)")
-
+    show_chapter(texts_dir, text_files, sections.index(add_selectbox))
 
 
 if __name__ == '__main__':
