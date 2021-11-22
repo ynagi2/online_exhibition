490eba9700202063c36e67d39d56fdfca5dcb706
YANAGIDA Yuki <ynagi2@dhcp219-129.slis.tsukuba.ac.jp>
2021-11-22 21:12:21 +0900

    add contact

--- exhibition.py
+++ exhibition.py
@@ -24,11 +24,13 @@ def show_chapter(texts_dir, text_files, idx):
 
 def main():
     st.title("コロナ禍におけるライブコンサートの変化（仮）")
+    st.markdown("代表者メールアドレス: s2121656 at s.tsukuba.ac.jp")
     texts_dir = "./text/"
     text_files = sorted(os.listdir(texts_dir))
-    sections = ("はじめに", "1章 コロナ禍以前・通常のライブ", "2章 コロナ禍の無観客・配信ライブ", "3章 後の対面", "おわりに")
+    sections = ("はじめに", "1章 コロナ禍以前・通常のライブ", "2章 コロナ禍の無観客・配信ライブ", "3章　有観客ライブの新様式", "おわりに", "担当者")
     add_selectbox = st.sidebar.selectbox("章立て", sections)
     show_chapter(texts_dir, text_files, sections.index(add_selectbox))
+    st.markdown("\n\n\n2021年11月　「人文知コミュニケーション：人文社会科学と自然科学の壁を超える」C班")
 
 
 if __name__ == '__main__':
